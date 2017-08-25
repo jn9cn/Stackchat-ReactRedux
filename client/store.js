997d05ca450500bcdd27c0c6aee95a55673123eb
jn9cn <jannine.chan@gmail.com>
2017-08-25 14:32:52 -0400

    connect complete

--- client/store.js
+++ client/store.js
@@ -10,7 +10,8 @@ import socket from './socket';
 const initialState = {
   messages: [],
   name: 'Reggie',
-  newMessageEntry: ''
+  newMessageEntry: '',
+  channels: []
 };
 
 // ACTION TYPES
@@ -19,6 +20,7 @@ const UPDATE_NAME = 'UPDATE_NAME';
 const GET_MESSAGE = 'GET_MESSAGE';
 const GET_MESSAGES = 'GET_MESSAGES';
 const WRITE_MESSAGE = 'WRITE_MESSAGE';
+const GET_CHANNELS = 'GET_CHANNELS';
 
 // ACTION CREATORS
 
@@ -42,6 +44,11 @@ export function writeMessage (content) {
   return action;
 }
 
+export function getChannels (channels) {
+  const action = { type: GET_CHANNELS, channels};
+  return action;
+}
+
 // THUNK CREATORS
 
 export function fetchMessages () {
@@ -55,6 +62,18 @@ export function fetchMessages () {
       });
   }
 }
+
+export function fetchChannels () {
+  
+    return function thunk (dispatch) {
+      return axios.get('/api/channels')
+        .then(res => res.data)
+        .then(channels => {
+          const action = getChannels(channels);
+          dispatch(action);
+        });
+    }
+  }
 
 export function postMessage (message) {
 
@@ -120,6 +139,12 @@ function reducer (state = initialState, action) {
       return {
         ...state,
         newMessageEntry: action.content
+      };
+
+    case GET_CHANNELS:
+      return {
+        ...state,
+        channels: action.channels
       };
 
     default:
